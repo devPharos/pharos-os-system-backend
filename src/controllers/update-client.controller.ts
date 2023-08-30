d873cc5bf87e46bee20a989953846f7f1ce70be4
taugitirana <thayna@pharosit.com.br>
2023-08-30 12:40:04 -0300

    feat: create update client controller

---
+++ src/controllers/update-client.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, HttpCode, Put, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const updateClientBodySchema = z.object({
+  userId: z.string().uuid(),
+  cnpj: z.string(),
+})
+
+type UpdateClientBodySchema = z.infer<typeof updateClientBodySchema>
+
+@Controller('/accounts')
+@UseGuards(JwtAuthGuard)
+export class UpdateClientController {
+  constructor(private prisma: PrismaService) {}
+  @Put('/client')
+  @HttpCode(201)
+  async handle(@Body() body: UpdateClientBodySchema) {
+    const { userId, cnpj } = body
+
+    await this.prisma.client.update({
+      where: { cnpj },
+      data: {
+        user: {
+          connect: { id: userId },
+        },
+      },
+    })
+  }
+}
